5c8f8cfde4407f4b2fec3e91c1b1a3ce455a11d1
akashsingh378 <akashsingh37823@gmail.com>
2024-12-22 22:07:09 +0530

    first commit

---
+++ three.js project/shaders/vertex.glsl
@@ -0,0 +1,7 @@
+// vertexShader.glsl
+varying vec2 vUv;
+
+void main() {
+    vUv = uv; // Pass the texture coordinates to the fragment shader
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
